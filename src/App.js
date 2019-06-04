6b62b29caa433cf4146ebf8ed8b10147570d00fd
Kevin <stradtkt22@gmail.com>
2019-06-04 18:22:41 -0400

    Added a spinner

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+import {Provider} from './context';
 
 function App() {
   return (
     <div className="App">
-        <Router>
-            <Fragment>
-                <Navbar/>
-                <div className="container">
-                    <Switch>
-                        <Route exact path='/' component={Index}/>
-                    </Switch>
-                </div>
-            </Fragment>
-        </Router>
+        <Provider>
+            <Router>
+                <Fragment>
+                    <Navbar/>
+                    <div className="container">
+                        <Switch>
+                            <Route exact path='/' component={Index}/>
+                        </Switch>
+                    </div>
+                </Fragment>
+            </Router>
+        </Provider>
     </div>
   );
 }
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Consumer} from '../../context';
+import Spinner from '../layout/Spinner';
 
 class Tracks extends Component {
     render() {
         return (
             <div>
-                <h1>Tracks</h1>
+                <Consumer>
+                    {value => {
+                        const {track_list} = value;
+                        if(track_list === undefined || track_list.length === 0) {
+                            return <Spinner/>
+                        } else {
+                            return <h1>Tracks</h1>;
+                        }
+                    }}
+                </Consumer>
             </div>
         );
     }
